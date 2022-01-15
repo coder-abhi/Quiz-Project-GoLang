5dcdc41b88081d3c6703a6b59a9331fdb892dc42
coder_abhi <abhi.kamble.3987@gmail.com>
2022-01-15 10:15:46 +0530

    More options added as math and other

--- index.go
+++ index.go
@@ -4,6 +4,7 @@ import (
 	"encoding/csv"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"strings"
 	"time"
@@ -11,9 +12,11 @@ import (
 
 func main() {
 
-	//                      Flag Name, Value       , Usage
-	csvFileName := flag.String("csv", "problems.csv", "Its is in formate of Quesion and Answers")
-	timeLimit := flag.Int("limit", 30, "Time limit for Quiz")
+	topic := flag.String("topic", "general", "On Which Topic you want to start Quiz, options -> math, general")
+
+	//                      Flag Name, Default Value   , Usage
+	// csvFileName := flag.String("csv", "prblems.csv", "Its is in formate of Quesion and Answers")
+	timeLimit := flag.Int("limit", 15, "Time limit for Quiz")
 	flag.Parse()
 	/* Use of Flag :-
 	It will now show anything when we run it simply
@@ -22,26 +25,24 @@ func main() {
 	But It will show 3rd parameter usage when we run it as help
 	like - go run index.go -h (or) -help
 	*/
-	_ = csvFileName
 
-	file, err := os.Open(*csvFileName)
-	if err != nil {
-		ExitProject("It cann't read the file!")
-	}
-	r := csv.NewReader(file)
-	lines, err := r.ReadAll()
-	if err != nil {
-		ExitProject("Unble to Read CSV")
-	}
+	var problemSet []problem
 
-	problems := parseProblem(lines)
-	fmt.Println(problems)
+	switch *topic {
+	case "math":
+		problemSet = MathQuiz(*timeLimit)
+
+	case "general":
+		problemSet = GeneralQuiz(*timeLimit)
+	}
+	// fmt.Println(*csvFileName)
 
 	var score int
 
 	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
 
-	for i, que := range problems {
+	// for loop for printing question and scanning every answer
+	for i, que := range problemSet {
 		fmt.Printf("\nProblem #%d : %s = ", i+1, que.q)
 		answerCh := make(chan string)
 		go func() {
@@ -50,12 +51,14 @@ func main() {
 			fmt.Scanf("%s\n", &answer)
 			answerCh <- answer
 		}()
+
+		// select funtion works like which channel give value first will execute
 		select {
-		case <-timer.C:
-			fmt.Printf("You Scored %d out of %d.", score, len(lines))
+		case <-timer.C: // Execute when limit time will over
+			fmt.Printf("\nYou Scored %d out of %d.", score, len(problemSet))
 			return
 
-		case answer := <-answerCh:
+		case answer := <-answerCh: // Execute when answer is given
 			if answer == que.a {
 				score++
 				fmt.Println("Correct!")
@@ -66,6 +69,51 @@ func main() {
 
 }
 
+func MathQuiz(limit int) []problem {
+
+	ret := make([]problem, 1)
+	return ret
+}
+
+func GeneralQuiz(limit int) []problem {
+
+	var fileList []string
+	files, err := ioutil.ReadDir(".")
+	if err != nil {
+		fmt.Println("Something is wrong, Program Can't Read files")
+	}
+
+	for _, file := range files {
+		if strings.HasSuffix(file.Name(), ".csv") {
+
+			fileList = append(fileList, file.Name())
+		}
+	}
+
+	fmt.Println("Choose Your Topic :")
+	var inputFile int
+
+	for i, file := range fileList {
+		fmt.Println(i+1, file)
+	}
+	fmt.Scanln(&inputFile)
+
+	csvFileName := fileList[inputFile-1]
+
+	file, err := os.Open(csvFileName)
+	if err != nil {
+		ExitProject("It cann't read the file!")
+	}
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		ExitProject("Unble to Read CSV")
+	}
+
+	return parseProblem(lines)
+}
+
+// Converting input in our formate ( in GeneralQuiz() )
 func parseProblem(lines [][]string) []problem {
 	ret := make([]problem, len(lines))
 
